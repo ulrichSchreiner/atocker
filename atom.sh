31674b040e3d2aa88e6ba87a109756966884df44
Ulrich Schreiner <ulrich.schreiner@gmail.com>
2015-10-25 21:50:16 +0100

    if there is a stored stage, use this when starting up

--- atom.sh
+++ atom.sh
@@ -5,4 +5,8 @@ export GO15VENDOREXPERIMENT=1
 
 /go/bin/gocode set package-lookup-mode gb
 
+if [ ! -d "/devhome/.atom/storage" ]; then
 cd /work && atom --foreground .
+else
+cd /work && atom --foreground
+fi
