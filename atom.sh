03378d16cfa2ddc87e119718414bc045cfbd970f
Ulrich Schreiner <ulrich.schreiner@gmail.com>
2016-02-24 09:41:52 +0100

    store config.cson in persistent area

--- atom.sh
+++ atom.sh
@@ -20,7 +20,10 @@ if [ -f "/devhome/.atom/config.cson" ]; then
   # remove old config.cson from previous version
   rm -rf /devhome/.atom/config.cson
 fi
-ln -s /devhome/config.cson /devhome/.atom/config.cson
+if [ ! -f "/config/atocker/config.cson" ]; then
+  cp /devhome/config.cson /config/atocker/config.cson
+fi
+ln -s /config/atocker/config.cson /devhome/.atom/config.cson
 
 if [ ! -d "/devhome/.atom/storage" ]; then
 cd /work && $ATOM --foreground .
